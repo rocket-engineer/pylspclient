161a3b7937c73dd0a773b79b5043d49ebecc3c1d
vistdn <tobias.dittmann@vector.com>
2023-09-29 16:22:11 +0200

    Purge clangd example

--- examples/clangd.py
+++ examples/clangd.py
@@ -9,17 +9,29 @@ class ReadPipe(threading.Thread):
         self.pipe = pipe
 
     def run(self):
-        line = self.pipe.readline().decode('utf-8')
+        line = self.pipe.readline().decode("utf-8")
         while line:
             print(line)
-            line = self.pipe.readline().decode('utf-8')
+            line = self.pipe.readline().decode("utf-8")
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='pylspclient example with clangd')
-    parser.add_argument('clangd_path', type=str, default="/usr/bin/clangd-6.0", 
-                    help='the clangd path', nargs="?")
+    parser = argparse.ArgumentParser(
+        description="pylspclient example with clangd")
+    parser.add_argument(
+        "clangd_path",
+        type=str,
+        default="/home/vistdn/.local/share/nvim/mason/bin/clangd",
+        help="the clangd path",
+        nargs="?",
+    )
     args = parser.parse_args()
-    p = subprocess.Popen([args.clangd_path], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen(
+        [args.clangd_path],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
     read_pipe = ReadPipe(p.stderr)
     read_pipe.start()
     json_rpc_endpoint = pylspclient.JsonRpcEndpoint(p.stdin, p.stdout)
@@ -27,121 +39,161 @@ if __name__ == "__main__":
     lsp_endpoint = pylspclient.LspEndpoint(json_rpc_endpoint)
 
     lsp_client = pylspclient.LspClient(lsp_endpoint)
-    capabilities = {'textDocument': {'codeAction': {'dynamicRegistration': True},
-    'codeLens': {'dynamicRegistration': True},
-    'colorProvider': {'dynamicRegistration': True},
-    'completion': {'completionItem': {'commitCharactersSupport': True,
-        'documentationFormat': ['markdown', 'plaintext'],
-        'snippetSupport': True},
-    'completionItemKind': {'valueSet': [1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        9,
-        10,
-        11,
-        12,
-        13,
-        14,
-        15,
-        16,
-        17,
-        18,
-        19,
-        20,
-        21,
-        22,
-        23,
-        24,
-        25]},
-    'contextSupport': True,
-    'dynamicRegistration': True},
-    'definition': {'dynamicRegistration': True},
-    'documentHighlight': {'dynamicRegistration': True},
-    'documentLink': {'dynamicRegistration': True},
-    'documentSymbol': {'dynamicRegistration': True,
-    'symbolKind': {'valueSet': [1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        9,
-        10,
-        11,
-        12,
-        13,
-        14,
-        15,
-        16,
-        17,
-        18,
-        19,
-        20,
-        21,
-        22,
-        23,
-        24,
-        25,
-        26]}},
-    'formatting': {'dynamicRegistration': True},
-    'hover': {'contentFormat': ['markdown', 'plaintext'],
-    'dynamicRegistration': True},
-    'implementation': {'dynamicRegistration': True},
-    'onTypeFormatting': {'dynamicRegistration': True},
-    'publishDiagnostics': {'relatedInformation': True},
-    'rangeFormatting': {'dynamicRegistration': True},
-    'references': {'dynamicRegistration': True},
-    'rename': {'dynamicRegistration': True},
-    'signatureHelp': {'dynamicRegistration': True,
-    'signatureInformation': {'documentationFormat': ['markdown', 'plaintext']}},
-    'synchronization': {'didSave': True,
-    'dynamicRegistration': True,
-    'willSave': True,
-    'willSaveWaitUntil': True},
-    'typeDefinition': {'dynamicRegistration': True}},
-    'workspace': {'applyEdit': True,
-    'configuration': True,
-    'didChangeConfiguration': {'dynamicRegistration': True},
-    'didChangeWatchedFiles': {'dynamicRegistration': True},
-    'executeCommand': {'dynamicRegistration': True},
-    'symbol': {'dynamicRegistration': True,
-    'symbolKind': {'valueSet': [1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        9,
-        10,
-        11,
-        12,
-        13,
-        14,
-        15,
-        16,
-        17,
-        18,
-        19,
-        20,
-        21,
-        22,
-        23,
-        24,
-        25,
-        26]}},'workspaceEdit': {'documentChanges': True},
-    'workspaceFolders': True}}
-    root_uri = 'file:///home/osboxes/projects/ctest/'
-    workspace_folders = [{'name': 'python-lsp', 'uri': root_uri}]
-    print(lsp_client.initialize(p.pid, None, root_uri, None, capabilities, "off", workspace_folders))
+    capabilities = {
+        "textDocument": {
+            "codeAction": {"dynamicRegistration": True},
+            "codeLens": {"dynamicRegistration": True},
+            "colorProvider": {"dynamicRegistration": True},
+            "completion": {
+                "completionItem": {
+                    "commitCharactersSupport": True,
+                    "documentationFormat": ["markdown", "plaintext"],
+                    "snippetSupport": True,
+                },
+                "completionItemKind": {
+                    "valueSet": [
+                        1,
+                        2,
+                        3,
+                        4,
+                        5,
+                        6,
+                        7,
+                        8,
+                        9,
+                        10,
+                        11,
+                        12,
+                        13,
+                        14,
+                        15,
+                        16,
+                        17,
+                        18,
+                        19,
+                        20,
+                        21,
+                        22,
+                        23,
+                        24,
+                        25,
+                    ]
+                },
+                "contextSupport": True,
+                "dynamicRegistration": True,
+            },
+            "definition": {"dynamicRegistration": True},
+            "documentHighlight": {"dynamicRegistration": True},
+            "documentLink": {"dynamicRegistration": True},
+            "documentSymbol": {
+                "dynamicRegistration": True,
+                "symbolKind": {
+                    "valueSet": [
+                        1,
+                        2,
+                        3,
+                        4,
+                        5,
+                        6,
+                        7,
+                        8,
+                        9,
+                        10,
+                        11,
+                        12,
+                        13,
+                        14,
+                        15,
+                        16,
+                        17,
+                        18,
+                        19,
+                        20,
+                        21,
+                        22,
+                        23,
+                        24,
+                        25,
+                        26,
+                    ]
+                },
+            },
+            "formatting": {"dynamicRegistration": True},
+            "hover": {
+                "contentFormat": ["markdown", "plaintext"],
+                "dynamicRegistration": True,
+            },
+            "implementation": {"dynamicRegistration": True},
+            "onTypeFormatting": {"dynamicRegistration": True},
+            "publishDiagnostics": {"relatedInformation": True},
+            "rangeFormatting": {"dynamicRegistration": True},
+            "references": {"dynamicRegistration": True},
+            "rename": {"dynamicRegistration": True},
+            "signatureHelp": {
+                "dynamicRegistration": True,
+                "signatureInformation": {
+                    "documentationFormat": ["markdown", "plaintext"]
+                },
+            },
+            "synchronization": {
+                "didSave": True,
+                "dynamicRegistration": True,
+                "willSave": True,
+                "willSaveWaitUntil": True,
+            },
+            "typeDefinition": {"dynamicRegistration": True},
+        },
+        "workspace": {
+            "applyEdit": True,
+            "configuration": True,
+            "didChangeConfiguration": {"dynamicRegistration": True},
+            "didChangeWatchedFiles": {"dynamicRegistration": True},
+            "executeCommand": {"dynamicRegistration": True},
+            "symbol": {
+                "dynamicRegistration": True,
+                "symbolKind": {
+                    "valueSet": [
+                        1,
+                        2,
+                        3,
+                        4,
+                        5,
+                        6,
+                        7,
+                        8,
+                        9,
+                        10,
+                        11,
+                        12,
+                        13,
+                        14,
+                        15,
+                        16,
+                        17,
+                        18,
+                        19,
+                        20,
+                        21,
+                        22,
+                        23,
+                        24,
+                        25,
+                        26,
+                    ]
+                },
+            },
+            "workspaceEdit": {"documentChanges": True},
+            "workspaceFolders": True,
+        },
+    }
+    root_path = "/home/vistdn/Misc/Tests/05_python/000_libclang_cindex/example/"
+    root_uri = "file://" + root_path
+    workspace_folders = [{"name": "python-lsp", "uri": root_uri}]
+    print(
+        lsp_client.initialize(
+            p.pid, None, root_uri, None, capabilities, "off", workspace_folders
+        )
+    )
     print(lsp_client.initialized())
 
     file_path = "/home/osboxes/projects/ctest/test.c"
